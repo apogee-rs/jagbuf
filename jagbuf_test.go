41688e98dacea3f92bf86685f731cdccc37d9f32
Justin Conner <jstncnnr@outlook.com>
2025-04-08 11:20:37 -0700

    Add test cases

---
+++ jagbuf_test.go
@@ -0,0 +1,87 @@
+package jagbuf
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestWrap(t *testing.T) {
+	data := []byte("test")
+	buffer := Wrap(data)
+
+	data = []byte("other")
+	if bytes.Equal(data, buffer.Bytes()) {
+		t.Error("Wrap fail: underlying data was changed")
+	}
+}
+
+func TestNewBufferWithCapacity(t *testing.T) {
+	buffer := NewWithCapacity(128)
+
+	if buffer.Capacity() != 128 {
+		t.Error("NewWithCapacity fail: underlying buffer capacity is wrong")
+	}
+}
+
+func TestBuffer_Grow(t *testing.T) {
+	buffer := NewWithCapacity(64)
+	buffer.Grow(64)
+
+	if buffer.Capacity() != 128 {
+		t.Error("NewWithCapacity fail: underlying buffer capacity is wrong")
+	}
+}
+
+func TestBuffer_Write(t *testing.T) {
+	buffer := NewWithCapacity(64)
+
+	data := []byte{0x0, 0x1, 0x2, 0x3}
+	_ = buffer.Write(data)
+
+	if !bytes.Equal(buffer.Bytes(), data) {
+		t.Error("Write fail: underlying buffer mismatch")
+	}
+}
+
+func TestBuffer_Slice(t *testing.T) {
+	buffer := Wrap([]byte{0x0, 0x1, 0x2, 0x3})
+
+	slice := buffer.Slice(1, 3)
+
+	buffer.writeIndex = 1
+	buffer.WriteUint8(0x0)
+	buffer.WriteUint8(0x0)
+
+	if bytes.Equal(slice, []byte{0x0, 0x0}) {
+		t.Error("Slice fail: slice was modified")
+	}
+}
+
+func TestBuffer_ReadString(t *testing.T) {
+	data := append([]byte("hello world"), 0x0)
+	buffer := Wrap(data)
+
+	str, err := buffer.ReadString()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if str != "hello world" {
+		t.Errorf("ReadString fail: Expected \"%s\" but received \"%s\"", "hello world", str)
+	}
+}
+
+func TestBuffer_ReadJagString(t *testing.T) {
+	data := append([]byte{0x0}, []byte("hello world")...)
+	data = append(data, 0x0)
+	buffer := Wrap(data)
+
+	str, err := buffer.ReadJagString()
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	if str != "hello world" {
+		t.Errorf("ReadJagString fail: Expected \"%s\" but received \"%s\"", "hello world", str)
+	}
+}
